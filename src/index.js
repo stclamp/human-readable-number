f99e02ccc77536bc61710c1f13683e48ec3ba1c5
Pavel <whatisclamp@gmail.com>
2022-06-22 19:41:18 +0300

    refactor: optimize solve

--- src/index.js
+++ src/index.js
@@ -1,137 +1,37 @@
 module.exports = function toReadable(number) {
     let readableNumber = ''
+    let num = number;
+    let firstSliced = num.toString().slice(0, 1);
+    let middleSliced = num.toString().slice(1, 2);
+    let lastSliced = num.toString().slice(2, num.length)
+    let middleAndLast = Number(middleSliced + lastSliced)
 
-    let firstSlisedNumber = number.toString().slice(0, 1);
-    let middleSlisedNumber = number.toString().slice(1, 2)
-    let lastSlisedNumber = number.toString().slice(2, number.length);
 
+    let toNineteen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let toNinety = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if (number <= 19 && number >= 1) {
-        readableNumber = numbersFromOneToFifteen(number);
-    } else if (number == 0) {
-        readableNumber = 'zero';
-    }
-    if (number >= 20 && number < 100) {
-        if (middleSlisedNumber == 0) {
-            readableNumber = numbersFromTwentyToNinety(Number(firstSlisedNumber)) + numbersFromOneToFifteen(Number(middleSlisedNumber))
-        } else if (middleSlisedNumber != 0) {
-            readableNumber = numbersFromTwentyToNinety(Number(firstSlisedNumber)) + ' ' + numbersFromOneToFifteen(Number(middleSlisedNumber))
+    if (num == 0) {
+        readableNumber = 'zero'
+    } else if (num >= 1 && num < 20) {
+        readableNumber = toNineteen[num]
+    } else if (num >= 20 && num < 100) {
+        (middleSliced == 0) ? readableNumber = toNinety[firstSliced - 1] : readableNumber = toNinety[firstSliced - 1] + ' ' + toNineteen[middleSliced];
+    } else if (num >= 100 && num <= 999) {
+        if (middleSliced == 0) {
+            readableNumber = toNineteen[firstSliced] + ' hundred ' + toNineteen[lastSliced]
+        } else if (middleSliced == 1 && (lastSliced >= 0 && lastSliced <= 9)) {
+            readableNumber = toNineteen[firstSliced] + ' hundred ' + toNineteen[middleAndLast]
+        } else if (lastSliced == 0 && middleSliced >= 2) {
+            readableNumber = toNineteen[firstSliced] + ' hundred ' + toNinety[middleSliced - 1]
+        } else {
+            readableNumber = toNineteen[firstSliced] + ' hundred ' + toNinety[middleSliced - 1] + ' ' + toNineteen[lastSliced]
         }
 
-    }
-
-    if (number >= 100 && number <= 999) {
-        if (middleSlisedNumber == 0) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred ' + numbersFromOneToFifteen(Number(lastSlisedNumber));
-        }
-        else if (lastSlisedNumber == 0) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred ' + numbersFromTwentyToNinety(Number(middleSlisedNumber)) + numbersFromOneToFifteen(Number(lastSlisedNumber))
-        } else if (middleSlisedNumber == 1 && (lastSlisedNumber >= 0 && lastSlisedNumber <= 9)) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred ' + numbersFromOneToFifteen(Number(number.toString().slice(1, number.length)))
-        } else if (lastSlisedNumber != 0) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred ' + numbersFromTwentyToNinety(Number(middleSlisedNumber)) + ' ' + numbersFromOneToFifteen(Number(lastSlisedNumber))
-        }
-        if (middleSlisedNumber == 0 && lastSlisedNumber == 0) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred'
-        }
-        if (middleSlisedNumber == 1 && lastSlisedNumber == 0) {
-            readableNumber = numbersFromOneToFifteen(Number(firstSlisedNumber)) + ' hundred ten'
+        if (middleSliced == 0 && lastSliced == 0) {
+            readableNumber = toNineteen[firstSliced] + ' hundred'
         }
     }
-    return readableNumber;
+
+    return readableNumber
 }
 
-function numbersFromOneToFifteen(number) {
-    let readableNumber = ''
-    switch (number) {
-        case 1:
-            readableNumber = 'one'
-            break;
-        case 2:
-            readableNumber = 'two'
-            break;
-        case 3:
-            readableNumber = 'three'
-            break;
-        case 4:
-            readableNumber = 'four'
-            break;
-        case 5:
-            readableNumber = 'five'
-            break;
-        case 6:
-            readableNumber = 'six'
-            break;
-        case 7:
-            readableNumber = 'seven'
-            break;
-        case 8:
-            readableNumber = 'eight'
-            break;
-        case 9:
-            readableNumber = 'nine'
-            break
-        case 10:
-            readableNumber = 'ten'
-            break
-        case 11:
-            readableNumber = 'eleven'
-            break
-        case 12:
-            readableNumber = 'twelve'
-            break
-        case 13:
-            readableNumber = 'thirteen'
-            break
-        case 14:
-            readableNumber = 'fourteen'
-            break
-        case 15:
-            readableNumber = 'fifteen'
-            break
-        case 16:
-            readableNumber = 'sixteen'
-            break
-        case 17:
-            readableNumber = 'seventeen'
-            break
-        case 18:
-            readableNumber = 'eighteen'
-            break
-        case 19:
-            readableNumber = 'nineteen'
-            break
-    }
-    return readableNumber;
-}
-
-function numbersFromTwentyToNinety(number) {
-    let readableNumber = ''
-    switch (number) {
-        case 2:
-            readableNumber = 'twenty'
-            break;
-        case 3:
-            readableNumber = 'thirty'
-            break;
-        case 4:
-            readableNumber = 'forty'
-            break;
-        case 5:
-            readableNumber = 'fifty'
-            break;
-        case 6:
-            readableNumber = 'sixty'
-            break;
-        case 7:
-            readableNumber = 'seventy'
-            break;
-        case 8:
-            readableNumber = 'eighty'
-            break;
-        case 9:
-            readableNumber = 'ninety'
-            break;
-    }
-    return readableNumber;
-}
